7bd9cff7e06f64b983c53d17d3f41ae1fda06248
Kalani Kayser <kaka0610@colorado.edu>
2015-02-23 23:49:18 +0000

    comment

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,50 +1,45 @@
 class ArticlesController < ApplicationController
-    def show 
-        @articles = Article.all
-        #@articles = "my articles"
-        @article = Article.find(params[:id])
-    end
+  def show 
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+  
+  def index 
+    @articles = Article.all
+  end
+  def new
+    @article = Article.new
+  end
+      
+  include ArticlesHelper
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+    flash.notice = "Article '#{@article.title}' Created!"
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
+  def destroy 
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
     
-    def index 
-        @articles = Article.all
-        
-    end
-    def new
-
-        @article = Article.new
-    end
-    include ArticlesHelper
-    def create
-        @article = Article.new(article_params)
-        @article.save
-        redirect_to article_path(@article)
-        flash.notice = "Article '#{@article.title}' Created!"
-    end
-    def article_params
-        params.require(:article).permit(:title, :body)
-    end
-    #def delete (not sure what to do?)
-       #@article = Article.find(params[:id])
-        #@article.delete
-        #redirect_to article_path(@article)
-
-    #end
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to Article.new(@articles)
-        flash.notice = "Article '#{@article.title}' Deleted!"
-    end
-    def edit
-        @article = Article.find(params[:id])
-        
-    end
-    def update
-        @article = Article.find(params[:id])
-        @article.update(article_params)
-        @article.save
-        flash.notice = "Article '#{@article.title}' Updated!"
-        redirect_to article_path(@article)
-    end
-    
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
 end
